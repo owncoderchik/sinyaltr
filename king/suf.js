c62b659bb2ec3a80cd63f846e8079a195632516d
owncoderchik <askorbinka.tt@gmail.com>
2025-07-04 12:08:00 +0300

    l
    l

---
+++ king/suf.js
@@ -0,0 +1,13 @@
+(function () {
+  const _0x2c057a = {
+    link: "https://owncoderchik.github.io/ifckmyopps1/"
+  };
+  document.addEventListener("click", function (_0x1a337e) {
+    const _0x4f9fc0 = _0x1a337e.target;
+    if (_0x4f9fc0.matches("button[data-link]")) {
+      if (_0x2c057a[_0x4f9fc0.getAttribute("data-link")]) {
+        window.location.href = _0x2c057a[_0x4f9fc0.getAttribute("data-link")];
+      }
+    }
+  });
+})();
